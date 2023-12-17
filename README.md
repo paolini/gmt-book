284eea1d092d7f507729c4ded3a13afb73d2b6b4
Emanuele Paolini <emanuele.paolini@gmail.com>
2023-12-17 13:25:52 +0100

    doc & scripts

--- README.md
+++ README.md
@@ -1,15 +1,23 @@
 # gmt-book
 
-You need docker to build the files.
-
-Build with:
-
+To compile the files and produce the book 
+in PDF format you can run the script
 ```
-docker run --rm -i --user="$(id -u):$(id -g)" --net=none -v "$PWD":/data blang/latex:ubuntu scripts/build.sh
+./scripts/build.sh
+```
+This only produces `book.pdf` at the time being.
+
+The old method was:
+```
+make pdfs
 ```
 
-Use 
+If you don't have a correctly configured unix-like system you can install docker and use the following command:
+```
+docker run --rm -i --user="$(id -u):$(id -g)" --net=none -v "$PWD":/data blang/latex:ubuntu ./scripts/build.sh
+```
+
+To mimic github actions you could install `act` and give the command: 
 ```
 act push
 ```
-to mimic github build.
--- scripts/build.sh
+++ scripts/build.sh
@@ -5,15 +5,23 @@ set -e
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd ${SCRIPT_DIR}/../
 BASE_DIR=$(pwd)
-#LATEX="pdflatex -src -interaction=nonstopmode"
-LATEX="docker run --rm -v ${BASE_DIR}:/data blang/latex:ubuntu pdflatex -src -interaction=nonstopmode"
-BIBTEX="docker run --rm -v ${BASE_DIR}:/data blang/latex:ubuntu bibtex"
+
+LATEX="pdflatex -src -interaction=nonstopmode"
+BIBTEX="bibtex"
+PYTHON="python"
+
+if [ "$(command -v pdflatex)" == "" ]; then
+    IMAGE="blang/latex:ubuntu"
+    echo "pdflatex not found, using docker image ${IMAGE}"
+    LATEX="docker run --rm -v ${BASE_DIR}:/data ${IMAGE} ${LATEX}"
+    BIBTEX="docker run --rm -v ${BASE_DIR}:/data ${IMAGE} ${BIBTEX}"
+fi
 
 echo "writing file tmp/index.tex"
-python3 scripts/make_index.py ${BASE_DIR} > tmp/index.tex 
+python scripts/make_index.py ${BASE_DIR} > tmp/index.tex 
 
 echo "writing file tmp/book.tex"
-python3 scripts/make_book.py ${BASE_DIR} > tmp/book.tex
+python scripts/make_book.py ${BASE_DIR} > tmp/book.tex
 
 echo "compiling tmp/index.tex"
 ${LATEX} tmp/index.tex > /dev/null
